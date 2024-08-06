5e2368ca10625a3b13b45e79501188621393d242
Vpyc <vpyc4408@gmail.com>
2024-08-06 13:15:41 +0300

    Сделана валидация и маска для телефона

--- test_task_SofTrust/ClientApp/src/app/app.module.ts
+++ test_task_SofTrust/ClientApp/src/app/app.module.ts
@@ -5,19 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import {FormComponent} from "./form/form.component";
-
+import {NgxMaskModule} from "ngx-mask";
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: FormComponent, pathMatch: 'full' },
-    ])
+      {path: '', component: FormComponent, pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test_task_SofTrust/ClientApp/src/app/form/form.component.ts
+++ test_task_SofTrust/ClientApp/src/app/form/form.component.ts
@@ -1,32 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-
 export class FormComponent implements OnInit {
   form: FormGroup;
+  submitted: boolean = false;
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      message: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.minLength(18)]],
       topic: ['', Validators.required],
       description: ['', Validators.required],
       captcha: ['', Validators.required]
     });
   }
 
+  ngOnInit(): void {}
+
   onSubmit() {
+    this.submitted = true;
     if (this.form.valid) {
       console.log(this.form.value);
+    } else {
+      console.log('Форма невалидна');
     }
   }
 
-  ngOnInit(): void {
+  get name() {
+    return this.form.get('name');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get phone() {
+    return this.form.get('phone');
+  }
+
+  get topic() {
+    return this.form.get('topic');
+  }
+
+  get description() {
+    return this.form.get('description');
+  }
+
+  get captcha() {
+    return this.form.get('captcha');
   }
 }
